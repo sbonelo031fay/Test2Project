e7f366ced32b263b615447069a6d6a95e0ee1cc9
SMM Faye <sibonelofaye031@gmail.com>
2024-03-25 11:11:23 +0200

    Adding my latest Question 3.1 and  3.2

---
+++ Question3.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+#Reading data from csv file
+foot_ball = pd.read_csv("C:/Users/Sibonelo Faye/Desktop/tp2_test_1/dataset - 2020-09-24.csv")
+print(foot_ball.tail(7))
--- registration.py
+++ registration.py
@@ -1,7 +1,7 @@
 
 party_list = []
 
-def register_party(party_name, reg_number, member_count):
+'''def register_party(party_name, reg_number, member_count):
     if member_count >= 1000:
         party_list.append({
             "party_name": party_name,
@@ -15,8 +15,22 @@ def register_party(party_name, reg_number, member_count):
         print("not eligible to Register!")
 
 
-register_party("MK", 10003319, 5300)
+register_party("MK", 10003319, 5300)'''
 
-        
+#question 2.3
+
+'''voter_id = {}
+def update_voter_info(voter_id):
+    "voter_id": {"name", "voting_district", "has_voted"}'''
+
+#question 2.4
+parties = ["ASC", "ATM", "AASD", "ANC", "AGANG SA", "ALJAMA", "AZAPO", "APC", "BRA", "BLF", "ZACP", "CPM", "CSA", "DOPE", "DA", "DLC","ECOFORUM", "EFF", "F4SD", "FREE DEMS"]
+
+my_parties = filter([n for n in parties if])
+
+    
+    
+
+
 
 
