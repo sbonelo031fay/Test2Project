b70b2ed41798d1cc184dba9aba59416413c3bfb3
SMM Faye <sibonelofaye031@gmail.com>
2024-03-25 10:21:52 +0200

    adding the question 2.1 and some screenshots to commit

--- registration.py
+++ registration.py
@@ -1,1 +1,22 @@
 print("Register to vote, your vote is your power to make a change")
+
+party_list = []
+
+def register_party(party_name, reg_number, member_count):
+    if member_count >= 1000:
+        party_list.append({
+            "party_name": party_name,
+            "reg_number": reg_number,
+            "member_count": member_count
+
+        })
+        print("Registered!")
+
+    else:
+        print("not eligible to Register!")
+
+register_party("nkata", 123, 500)
+
+        
+
+
